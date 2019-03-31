e85b53386667a3f1300bc362eb62c8c6a832ae62
Tiago Marques <thymarques@gmail.com>
2019-03-31 09:28:37 -0700

    Removing react-tabs, and adding Material-UI tabs. Adding Header component.

--- src/App.js
+++ src/App.js
@@ -1,47 +1,92 @@
 import React, { Component } from 'react';
 
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-import "react-tabs/style/react-tabs.css";
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
 
 import Store from './Store/Store'
+import Header from './components/Header'
 import Tooth from './components/Tooth'
 import Toolbar from './components/Toolbar'
 
 
 import './App.css';
 
+
+const TabContainer = (props) => {
+  return (
+    <Typography component="div" style={{ padding: 8 * 3 }}>
+      {props.children}
+    </Typography>
+  );
+}
+
+TabContainer.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+const styles = (theme) => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+  },
+});
+
+
+
+
 class App extends Component {
   constructor(props){
     super(props)
 
     this.state = Store
+
+    this.state.value = 0
   }
 
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
+
   render() {
+
+    const { classes } = this.props;
+    const { value } = this.state;
+
     return (
       <div className="container">
-        <header>
-          <h3>Ready to start programming the OdontogramaJS.</h3>
-        </header>
-
+        
+        <Header />
         <main>
-          <Tabs>
-            <TabList>
-              <Tab>Adulto</Tab>
-              <Tab>Infantil</Tab>
-            </TabList>
+          <div className={classes.root}>
 
-            <TabPanel>
+            <AppBar position="static">
+              <Tabs value={value} onChange={this.handleChange}>
+                <Tab label="Adulto" />
+                <Tab label="Infantil" />
+              </Tabs>
+            </AppBar>
+
+            {value === 0 && 
+              <TabContainer>
                 {this.state.arcada.adulto.map((item) => {
                   return <Tooth key={item.id} data={item}/>
                 })}
-            </TabPanel>
-            <TabPanel>
-            {this.state.arcada.infantil.map((item) => {
-              return <Tooth key={item.id} data={item}/>
-            })}
-            </TabPanel>
-          </Tabs>
+              </TabContainer>
+            }
+
+            {value === 1 && 
+              <TabContainer>
+                {this.state.arcada.infantil.map((item) => {
+                  return <Tooth key={item.id} data={item}/>
+                })}
+              </TabContainer>
+            }
+          </div>
+
           <Toolbar toolbar={this.state.toolbar}/>
         </main>
         
@@ -50,4 +95,8 @@ class App extends Component {
   }
 }
 
-export default App;
+App.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(App);
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Typography from '@material-ui/core/Typography';
+
+export default function Header() {
+  return (
+    <header className="header">
+        <Typography variant="h4" align="center" gutterBottom>
+            Ready to start programming the OdontogramaJS.
+        </Typography>
+    </header>
+  )
+}
